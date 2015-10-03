9400b800ed0dd503cde35f9df9da68edb87e33c1
Khandakar Mamun <shohad.cse@gmail.com>
2015-10-03 16:04:03 -0300

    Profile commit

--- app/src/main/java/com/easygo/rbcdev/easygo/ProfileActivity.java
+++ app/src/main/java/com/easygo/rbcdev/easygo/ProfileActivity.java
@@ -5,23 +5,36 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class ProfileActivity extends Activity {
+
+    private Button paymentInfo;
+    private LinearLayout paymentLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        initialize();
     }
 
+    private void initialize(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
+        paymentInfo = (Button)findViewById(R.id.btnPaymentInfo);
+        paymentLayout = (LinearLayout)findViewById(R.id.paymentInfo);
+        paymentInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentLayout.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
