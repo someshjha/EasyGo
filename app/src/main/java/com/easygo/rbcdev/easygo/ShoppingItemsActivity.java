d0d5d3c58c4392df6f80ff07ddb57e9a44459586
Dennis Hall <dhall@ca.ibm.com>
2015-10-03 10:44:54 -0300

    Product Details Page, Shopping Home, Updated Icons, etc

--- app/src/main/java/com/easygo/rbcdev/easygo/ShoppingItemsActivity.java
+++ app/src/main/java/com/easygo/rbcdev/easygo/ShoppingItemsActivity.java
@@ -2,37 +2,43 @@ package com.easygo.rbcdev.easygo;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.easygo.rbcdev.easygo.widgets.Header;
 
 
-public class ShoppingHome extends Activity {
+public class ShoppingItemsActivity extends Activity {
 
+    private Spinner mLstCategoryList;
+    private ArrayAdapter<CharSequence> itemAdapter;
+    private Button mBtnViewCart;
     private Header mHeader;
-    private Button mBtnStartShopping;
 
     private View.OnClickListener settingsListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-           goToSettings();
+            goToSettings();
         }
     };
 
-    private View.OnClickListener startShoppingListener = new View.OnClickListener() {
+    private View.OnClickListener productDetailsListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            goToShopping();
+            goToProductDetails();
         }
     };
 
-    private void goToShopping() {
-        Intent i = new Intent(this,ShoppingItemsActivity.class);
+    private void goToProductDetails() {
+        Intent i = new Intent(this,ProductDetailsActivity.class);
         startActivity(i);
     }
 
@@ -44,22 +50,31 @@ public class ShoppingHome extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_home);
+        setContentView(R.layout.activity_shopping_items);
         initializeUI();
+        populateLists();
+    }
+
+    private void populateLists() {
+        itemAdapter = ArrayAdapter.createFromResource(this, R.array.items, android.R.layout.simple_list_item_1);
+        itemAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        mLstStoreList.setAdapter(itemAdapter);
+        mLstCategoryList.setAdapter(itemAdapter);
     }
 
     private void initializeUI() {
+//        mLstStoreList = (Spinner) findViewById(R.id.StoreList);
+        mLstCategoryList = (Spinner) findViewById(R.id.CategoryList);
         mHeader = (Header) findViewById(R.id.header);
         mHeader.setBtnRightListener(settingsListener);
-        mBtnStartShopping = (Button) findViewById(R.id.btnStartShopping);
-        mBtnStartShopping.setOnClickListener(startShoppingListener);
+        mBtnViewCart = (Button) findViewById(R.id.btnViewCartItems);
+        mBtnViewCart.setOnClickListener(productDetailsListener);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_shopping_home, menu);
+        getMenuInflater().inflate(R.menu.menu_shopping_items, menu);
         return true;
     }
 
