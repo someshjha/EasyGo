770d545348864477269ce10e861cd68e209abaf0
Somesh Jha <someshj@ca.ibm.com>
2015-10-04 15:24:19 -0300

    Business Page Home

--- app/src/main/java/com/easygo/rbcdev/easygo/BusinessHome.java
+++ app/src/main/java/com/easygo/rbcdev/easygo/BusinessHome.java
@@ -1,40 +1,69 @@
 package com.easygo.rbcdev.easygo;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class BusinessHome extends Activity {
+
+    private TextView employeeEval;
+    private TextView progressReport;
+    private TextView quaterlySales;
+    private TextView annualSales;
+    private TextView leaveRequest;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_home);
+        initialize();
+    }
+
+    private void initialize(){
+
+        employeeEval = (TextView)findViewById(R.id.employeeEval);
+        employeeEval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        progressReport = (TextView)findViewById(R.id.progressReport);
+        progressReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        quaterlySales = (TextView)findViewById(R.id.quaterlySales);
+        quaterlySales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        annualSales = (TextView)findViewById(R.id.annualSales);
+        annualSales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        leaveRequest = (TextView)findViewById(R.id.leaveRequest);
+        leaveRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_business_home, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
