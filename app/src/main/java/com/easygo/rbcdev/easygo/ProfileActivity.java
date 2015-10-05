4be87fddfcf89e0dd3b77899a7574907b9e841fd
Dennis Hall <dhall@ca.ibm.com>
2015-10-05 16:23:47 -0300

    Prepping registrater functionality

--- app/src/main/java/com/easygo/rbcdev/easygo/ProfileActivity.java
+++ app/src/main/java/com/easygo/rbcdev/easygo/ProfileActivity.java
@@ -1,29 +1,68 @@
 package com.easygo.rbcdev.easygo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import com.easygo.rbcdev.easygo.models.Constants;
+import com.easygo.rbcdev.easygo.widgets.CustomField;
+import com.easygo.rbcdev.easygo.widgets.Header;
 
 
 public class ProfileActivity extends Activity {
 
     private Button paymentInfo;
     private LinearLayout paymentLayout;
+    private LinearLayout mLayoutPasswordChange;
+    private CustomField mNewPasswordLabel;
+    private CustomField mConfirmPasswordLabel;
+    private EditText mNewPassword;
+    private EditText mConfirmPassword;
+    private Header mHeader;
+    private Intent i;
 
+    private View.OnClickListener backListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        i = getIntent();
+        initializeUI();
+        showHidePasswordFields();
 //        initialize();
+    }
 
+    private void showHidePasswordFields() {
+        String loginType = i.getStringExtra("loginType");
+        if(loginType.equals(Constants.SOBEYS_GUEST)){
+            mLayoutPasswordChange.setVisibility(View.GONE);
+            //Make profile info password fields and labels visible
+        }
+        else {
+            //hide profile info password fields and labels
+            mLayoutPasswordChange.setVisibility(View.VISIBLE);
+        }
 
+    }
 
+    private void initializeUI() {
+        mHeader = (Header) findViewById(R.id.header);
+        mHeader.setBtnLeftListener(backListener);
+        mLayoutPasswordChange = (LinearLayout) findViewById(R.id.ChangePasswordInformation);
+        //initialize new password fields here
     }
 
 //    private void initialize(){
