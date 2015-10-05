d2f72f76281c4e3a485afe1292e0932e8f90af04
Somesh Jha <someshj@ca.ibm.com>
2015-10-05 10:53:42 -0300

    Business end

--- app/src/main/java/com/easygo/rbcdev/easygo/BusinessHome.java
+++ app/src/main/java/com/easygo/rbcdev/easygo/BusinessHome.java
@@ -1,13 +1,17 @@
 package com.easygo.rbcdev.easygo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.easygo.rbcdev.easygo.BusinessEnd.LeaveRequest;
+
 
 public class BusinessHome extends Activity {
 
+    private Activity mActivity = this;
     private TextView employeeEval;
     private TextView progressReport;
     private TextView quaterlySales;
@@ -29,37 +33,48 @@ public class BusinessHome extends Activity {
         employeeEval.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                employeeEval.setBackgroundColor(getResources().getColor(R.color.blue_btn_pressed));
             }
         });
         progressReport = (TextView)findViewById(R.id.progressReport);
         progressReport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                progressReport.setBackgroundColor(getResources().getColor(R.color.blue_btn_pressed));
             }
         });
         quaterlySales = (TextView)findViewById(R.id.quaterlySales);
         quaterlySales.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                quaterlySales.setBackgroundColor(getResources().getColor(R.color.sobeys_green));
             }
+
         });
         annualSales = (TextView)findViewById(R.id.annualSales);
         annualSales.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                annualSales.setBackgroundColor(getResources().getColor(R.color.blue_btn_pressed));
+            }
+        });
+        annualSales.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                quaterlySales.setBackgroundColor(getResources().getColor(R.color.sobeys_green));
 
+                return false;
             }
         });
         leaveRequest = (TextView)findViewById(R.id.leaveRequest);
         leaveRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(mActivity, LeaveRequest.class);
+                startActivity(intent);
             }
         });
+
 
     }
 
