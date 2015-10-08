1176fa3e4177d3f2a41e50dbe15862acf8411c62
Khandakar Mamun <shohad.cse@gmail.com>
2015-10-08 01:22:11 -0300

    Updated update password button click, worked on Cart, Item model, created ShoppingHistory model

--- app/src/main/java/com/easygo/rbcdev/easygo/models/Cart.java
+++ app/src/main/java/com/easygo/rbcdev/easygo/models/Cart.java
@@ -1,15 +1,38 @@
 package com.easygo.rbcdev.easygo.models;
 
 import com.activeandroid.Model;
-
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by rbcdev on 15-10-06.
  */
+
+@Table(name = "Cart")
 public class Cart extends Model implements Serializable {
 
     private ArrayList<Item> cartItems;
+
+    public String get_item() {
+        return _item;
+    }
+
+    public void set_item(String _item) {
+        this._item = _item;
+    }
+
+    private String _item;
+
+
+    public ArrayList<Item> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(ArrayList<Item> cartItems) {
+        this.cartItems = cartItems;
+    }
+
 
 }
---
+++ app/src/main/java/com/easygo/rbcdev/easygo/models/ShoppingHistory.java
@@ -0,0 +1,63 @@
+package com.easygo.rbcdev.easygo.models;
+
+import com.activeandroid.Model;
+
+import java.io.Serializable;
+
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+/**
+ * Created by MyWorkPlace on 15-10-08.
+ */
+
+@Table(name = "ShoppingHistory")
+
+public class ShoppingHistory extends Model implements Serializable {
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "customerEmail")
+    private String customerEmail;
+
+    @Column(name = "store")
+    private String store;
+
+    @Column(name = "total")
+    private String total;
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+
+}
